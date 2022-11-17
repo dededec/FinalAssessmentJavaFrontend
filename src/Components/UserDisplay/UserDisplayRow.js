e92c6196d259591bb01f34422d78cd07477d243b
dededec <daviderenacasado@gmail.com>
2022-11-17 15:50:46 +0100

    Delete button done

--- src/Components/UserDisplay/UserDisplayRow.js
+++ src/Components/UserDisplay/UserDisplayRow.js
@@ -1,19 +1,23 @@
 import { useContext } from "react";
 import UserContext from "../../Context/userContext";
+import { deleteUser } from "../../Api/userFetch";
 import Card from "../Card/Card";
 
 function UserDisplayRow({ user }) {
-	return (
-		<Card>
-			<div class="row">
-				<div class="col">{user.id}</div>
-				<div class="col">{user.firstName}</div>
-				<div class="col">{user.lastName}</div>
-				<div class="col">{user.phoneNumber}</div>
-				<div class="col">{user.email}</div>
-			</div>
-		</Card>
-	);
+    return (
+        <Card>
+            <div className="row">
+                <div className="col">{user.id}</div>
+                <div className="col">{user.firstName}</div>
+                <div className="col">{user.lastName}</div>
+                <div className="col">{user.phoneNumber}</div>
+                <div className="col">{user.email}</div>
+                <div className="col">
+                    <button type="button" onClick={() => deleteUser(user.id).catch(error => console.log("Este error no influye: " + error))} className="btn btn-danger">Delete</button>
+                </div>
+            </div>
+        </Card>
+    );
 }
 
 export default UserDisplayRow;
